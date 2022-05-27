4f896efec4a92dc0a13ce21447fd6115ed727845
GARM2021 <ivanhoe.real@gmail.com>
2022-05-27 18:21:43 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+ form = document.getElementById("form");
+ input = document.getElementById("input");
+ msg = document.getElementById("msg");
+ posts = document.getElementById("posts");
+
+
+ form.addEventListener("submit", (e) => {
+     e.preventDefault();
+     console.log("button clicked");
+
+     formValidation();
+ });
+
+
+
+ formValidation = () => {
+     if (input.value === "") {
+         msg.innerHTML = "Post cannot be blank";
+         console.log("failure");
+     } else {
+         acceptData();
+     }
+ };
+
+ let data = {};
+
+ let acceptData = () => {
+     data["text"] = input.value;
+     console.log(data);
+     createPost();
+ };
+
+ let createPost = () => {
+     posts.innerHTML += `
+    <div>
+      <p>${data.text}</p>
+      <span class="options">
+        <i onClick="editPost(this)" class="fas fa-edit"></i>
+        <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+      </span>
+    </div>
+    `;
+     input.value = "";
+ };
---
+++ readme_notas.php
@@ -0,0 +1,13 @@
+20220527
+fontawesome.com 
+
+la etiqueta script debe ir despues del la etiqueta body 
+
+
+Con esta instruccion adiciono HTML a la pagina 
+
+posts.innerHTML += ` 
+   <div>
+    <p>${data.text}</p>
+        ... codigo ... 
+        `;
